8d15087b645db22a73cea82f65aad96f7a176071
Abhishek Patel <Abhi882731@gmail.com>
2021-08-02 03:19:19 -0700

    Updates

---
+++ precache-manifest.66cba4037ca530e686d094b32429bdf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4994379328b09c56a49f1f6fa5a68f",
+    "url": "/cryptosite.github.io/index.html"
+  },
+  {
+    "revision": "3cd878afdc3daf284373",
+    "url": "/cryptosite.github.io/static/css/2.39596b15.chunk.css"
+  },
+  {
+    "revision": "5a9153fa187bed7067f7",
+    "url": "/cryptosite.github.io/static/css/main.ad838ff9.chunk.css"
+  },
+  {
+    "revision": "3cd878afdc3daf284373",
+    "url": "/cryptosite.github.io/static/js/2.7a00cb5b.chunk.js"
+  },
+  {
+    "revision": "5a9153fa187bed7067f7",
+    "url": "/cryptosite.github.io/static/js/main.79e1e9ad.chunk.js"
+  },
+  {
+    "revision": "f4efb4393284b8ed0661",
+    "url": "/cryptosite.github.io/static/js/runtime-main.b0c8172f.js"
+  }
+]);
